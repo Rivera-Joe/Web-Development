02352d807367ba23eb7454e08c278918882d2f29
Rivera-Joe <36744600+Rivera-Joe@users.noreply.github.com>
2019-09-25 19:00:03 -0500

    finished lab 2
    finished mobile apps lab 2

--- html/Mobile Apps/Lab 2/javascript/main.js
+++ html/Mobile Apps/Lab 2/javascript/main.js
@@ -2,3 +2,38 @@ function pageChange(page){
 
     page == 'form' ? location.href="./form.html" : location.href="./index.html";
 }
+
+
+function showValue(){
+var value = document.querySelector('#slider').value;
+document.querySelector('#output').innerHTML = value;
+}
+
+
+// store information for second page use
+
+
+// function save()
+// {
+//     var fName = document.querySelector('#fName').value;
+//     var LName = document.querySelector('#lName').value;
+//     var bDay = document.querySelector('#bDay').value;
+//     var tutor = document.querySelector('#tutor').value;
+
+//     var userData = `${fName} ${LName}<br />${bDay}<br />${tutor}`;
+//     return userData;
+
+
+    
+// }
+
+
+
+// document.querySelector('#data').innerHTML = save();
+
+    
+    
+
+
+
+

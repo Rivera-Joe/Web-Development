ff6213fbf56a13e42ef9868776f370b93a520fef
Rivera-Joe <36744600+Rivera-Joe@users.noreply.github.com>
2019-09-26 08:58:14 -0500

    Mobile Apps 2 C
    Completed Form mods for lab 2.
    removed second page

--- html/Mobile Apps/Lab 2/javascript/main.js
+++ html/Mobile Apps/Lab 2/javascript/main.js
@@ -9,28 +9,44 @@ var value = document.querySelector('#slider').value;
 document.querySelector('#output').innerHTML = value;
 }
 
+function hideUserData(){
+    document.querySelector('#userAlert').style.visibility = "hidden";
+}
 
-// store information for second page use
 
+function setup(){
 
-// function save()
-// {
-//     var fName = document.querySelector('#fName').value;
-//     var LName = document.querySelector('#lName').value;
-//     var bDay = document.querySelector('#bDay').value;
-//     var tutor = document.querySelector('#tutor').value;
+    location.href="#";
 
-//     var userData = `${fName} ${LName}<br />${bDay}<br />${tutor}`;
-//     return userData;
+     var fName = document.querySelector('#fName').value;
+     var LName = document.querySelector('#lName').value;
+     var bDay = document.querySelector('#bDay').value;
+     var tutor = document.querySelector('#tutor').value;
+     var color = document.querySelector('#color').value;
 
+    document.querySelector('.formStuff').style.visibility = "hidden";
+
+    document.querySelector('#label').innerHTML = `Welcome ${fName} ${LName}!`;
+
+    document.querySelector('#firstLine').innerHTML = `D.O.B: ${bDay}<br />Tutor: ${tutor}<br />Color: ${color}`;
+
+    document.body.style.backgroundColor = color;
+    var userData = document.querySelector('.user');
+    userData.style.backgroundColor = "#333333";
+    userData.style.marginTop = '1em';
+    userData.style.paddingTop = '.8em';
+    userData.style.paddingBottom = '.8em';
+    userData.style.textAlign = "center";
+
+    var alertUser = document.querySelector('#userAlert');alertUser.style.visibility = 'visible';
+    alertUser.style.paddingTop = "1em";
 
     
-// }
 
 
 
-// document.querySelector('#data').innerHTML = save();
 
+}
     
     
 
